3f52577860b083d5a33453483794a8425f988fc6
sacramento <ricardo.sacramento18@gmail.com>
2024-04-13 14:34:59 +0100

    Save state in course

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+enum Color {
+  Red,
+  Green,
+  Yellow,
+  Blue,
+}
+
+function printColor(color: Color) {
+  switch (color) {
+    case Color.Red:
+      console.log("red");
+    break;
+    case Color.Green:
+      console.log("green");
+      break;
+    case Color.Blue:
+      console.log("blue");
+      break;
+  }
+}
+
+printColor(Color.Yellow);
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+enum Color {
+    Red,
+    Green,
+    Blue,
+    Yellow,
+}
+
+fn print_color(color: Color) -> () {
+    match color {
+        Color::Red => println!("red"),
+        Color::Green => println!("green"),
+        Color::Blue => println!("blue"),
+        Color::Yellow=> println!("yellow"),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_color(Color::Yellow);
 }
